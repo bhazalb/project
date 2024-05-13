16ed6e9c2c1ba1fc60b154ac49b5ef3b8185581e
bhazalb <bayramhazal030@gmail.com>
2024-05-13 23:39:10 +0300

    kütüphane

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState , useEffect} from "react";
 import {fetchDataFromApi} from "./utils/api";
 
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
+
 function App() {
+
+  const dispatch = useDispatch()
+
+   const url = useSelector((state) => state.url );
+
+
+
   useEffect(() =>{
     apiTesting();
   }  , []);
@@ -9,11 +19,17 @@ function App() {
 const apiTesting = () => {
   fetchDataFromApi("/movie/popular").then((res) => {
     console.log(res)
+    dispatch(getApiConfiguration(res))
   });
  
 };
 
-  return <div className="App"> App </div>;
+  return (
+  <div className="App">
+         App 
+        {url?.total_pages}
+        </div>
+  );
 }
 export default App;
 
