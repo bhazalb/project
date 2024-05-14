3aa26a687cda28640f67a2b05f296262d2de8b1d
bhazalb <bayramhazal030@gmail.com>
2024-05-14 13:03:35 +0300

    update

---
+++ movie-app/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import  "./style.scss";
+import HeroBanner from "./HeroBanner/HeroBanner";
+
+
+HeroBanner
+
+const Home = () => {
+    return (
+       <div className="homePage">
+        <HeroBanner></HeroBanner>
+       </div>
+    )
+}
+
+export default Home
